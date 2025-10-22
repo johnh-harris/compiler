d57cd93e1051caf820c5db7e4dfa3952be1f690a
johnh-harris <39563774+johnh-harris@users.noreply.github.com>
2025-10-22 14:36:37 -0400

    Initial commit

---
+++ PrattParser.java
@@ -0,0 +1,9 @@
+public class PrattParser {
+    public static void main(String[] args) {
+        // grab tokens this will be a file path
+        Lexer lexer = new Lexer("ADD 1 3");
+        Parser parser = new Parser(lexer);
+        Node result = parser.parseExpression(0);
+        System.out.println("Parsed: " + result);
+    }
+}
---
+++ Token.java
@@ -0,0 +1,15 @@
+// Returns output file of tokens
+
+class Token {
+    public final String type;
+    public final String value;
+    public final int line;
+    public Token next;
+
+    public Token(String type, String value, int line, Token next) {
+        this.type = type;
+        this.value = value;
+        this.next = next;
+        this.line = line;
+    }
+}
