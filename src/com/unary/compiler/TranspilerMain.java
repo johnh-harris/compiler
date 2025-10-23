5edf6c0cfea3cadffd008c13929b751b9c8e3170
johnh-harris <39563774+johnh-harris@users.noreply.github.com>
2025-10-23 00:55:33 -0400

    Fixed comments

--- src/com/unary/compiler/TranspilerMain.java
+++ src/com/unary/compiler/TranspilerMain.java
@@ -1,17 +1,14 @@
-
-// === Step 3: Example Main (Parser + Transpiler together) ===
 public class TranspilerMain {
     public static void main(String[] args) {
-        // This assumes you already have a Lexer + Parser
-        Lexer lexer = new Lexer("1 + 2 * 3");
+        /*Lexer lexer = new Lexer("1 + 2 * 3");
         Parser parser = new Parser(lexer);
         Node ast = parser.parseExpression(0);
-
+	*/
         // Now transpile to unary instructions
-        Transpiler transpiler = new Transpiler();
+        /*Transpiler transpiler = new Transpiler();
         String unaryCode = transpiler.transpile(ast);
 
         System.out.println("=== Unary Code ===");
-        System.out.println(unaryCode);
+        System.out.println(unaryCode);*/
     }
 }
